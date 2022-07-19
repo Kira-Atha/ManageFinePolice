a0bda3f6d9be2702d83f08f06bdbe7f741e6730f
Gabriel <gaabriel.08@hotmail.com>
2022-07-19 19:18:29 +0200

    Create Insert lines

--- SGBD/Usely/Script-Police-Insert.sql
+++ SGBD/Usely/Script-Police-Insert.sql
@@ -4,20 +4,18 @@ INSERT INTO VIOLATION VALUES(1,'Flash light',150,'The charged didnt use his flas
 INSERT INTO VIOLATION VALUES(2,'Safety belt',150,'The charged didnt put his safety belt');
 INSERT INTO VIOLATION VALUES(3,'Phone',200,'The charged used his phone while he was driving');
 
-INSERT INTO PurchaseDepartment VALUES(1,'Marchal_Pur','MarchalMarchalPurTruite','Marchal');
-INSERT INTO PurchaseDepartment VALUES(2,'Delire_Pur','DelireDelirePurGrenouille','Delire');
+INSERT INTO CHARGED VALUES(1,'John','Doe','02 Rue du coquelicot');
+INSERT INTO CHARGED VALUES(2,'Johnny','Hoy','08 Rue du cimetière');
 
-INSERT INTO Machine VALUES(1,'XE00',1,2,1);
-INSERT INTO Machine VALUES(2,'XB00',2,1,1);
+INSERT INTO TYPEVEHICLE VALUES(1,'Car');
+INSERT INTO TYPEVEHICLE VALUES(2,'Moto');
+INSERT INTO TYPEVEHICLE VALUES(3,'Truck');
 
-INSERT INTO Site_Mach VALUES(1,1);
-INSERT INTO Site_Mach VALUES(2,1);
-INSERT INTO Site_Mach VALUES(1,2);
+INSERT INTO REGISTRATION VALUES(1,'0-UGN-201');
 
-INSERT INTO Maintenance(IDMAINTENANCE,DATE_START,ISSUE_DESCRIPTION,STATUS_MAINTENANCE,IDMANAGER,IDMACHINE)
-	VALUES(1,to_date('20-12-2021','dd-mm-yyyy'),'This machine makes a loudly noise when it started',1,1,1);
+INSERT INTO VEHICLE VALUES(1,1,1);
+INSERT INTO FINE VALUES(1,to_date('20-12-2021','dd-mm-yyyy'),'Call to order ignored',0,1,1,1);
 
-INSERT INTO maint_worker VALUES(1,2);
-INSERT INTO maint_worker VALUES(1,3);
+INSERT INTO VIO_FIN VALUES(3,1);
 
 commit;
